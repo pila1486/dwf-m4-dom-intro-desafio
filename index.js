2b4870db8747ba7d4a86ce5bf2430b582adb9a67
pila1486 <101686992+pila1486@users.noreply.github.com>
2023-06-25 13:22:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,18 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
-
+function main() {
+  const lista = document.querySelector(".lista");
+  const liEls = lista.querySelectorAll("li");
+  for (let itemAEliminar of liEls) {
+    itemAEliminar.remove();
+  }
+  for (let item of cosasQueAprendimos) {
+    const newLiEl = document.createElement("li");
+    newLiEl.textContent = item.tema;
+    newLiEl.classList.add(item.class || "item");
+    lista.appendChild(newLiEl);
+  }
+}
 main();
+
